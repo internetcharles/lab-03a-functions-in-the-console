82d3efc82d63db21a56bb26a6f70c5e393da3305
internetcharles <charlesodellsmith@gmail.com>
2020-03-25 16:19:17 -0700

    Step 3: first function complete

--- app.js
+++ app.js
@@ -3,11 +3,18 @@ const dave = { name: 'dave', age: 3, weight: 14};
 const jorn = { name: 'jorn', age: 5, weight: 18};
 const brandon = { name: 'brandon', age: 85, weight: 45};
 const mondo = { name: 'mondo', age: 12, weight: 13};
+const johnny = {name: 'johnny', age: 10, weight: 32};
 
-const cats = [felix, dave, jorn, brandon, mondo];
+const cats = [felix, dave, jorn, brandon, mondo, johnny];
 
 
 for(let i = 0; i < cats.length; i++) {
     const cat = cats[i];
-    console.log(cat)
+    console.log(cat);
 }
+
+function logOutThings(n) {
+    console.log('Here are your ' + n.length + ' things!');
+}
+
+logOutThings(cats);
