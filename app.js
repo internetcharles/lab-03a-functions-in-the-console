496a5836ec33c0a39fbaad6b58beb216acef1d2c
internetcharles <charlesodellsmith@gmail.com>
2020-03-25 16:10:34 -0700

    First step: array looped and logged to console

---
+++ app.js
@@ -0,0 +1,13 @@
+const felix = { name: 'felix', age: 13, weight: 20};
+const dave = { name: 'dave', age: 3, weight: 14};
+const jorn = { name: 'jorn', age: 5, weight: 18};
+const brandon = { name: 'brandon', age: 85, weight: 45};
+const mondo = { name: 'mondo', age: 12, weight: 13};
+
+const cats = [felix, dave, jorn, brandon, mondo];
+
+
+for(let i = 0; i < cats.length; i++) {
+    const cat = cats[i];
+    console.log(cat)
+}
